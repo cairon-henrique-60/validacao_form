b87682408aa132288df8f662bdaba01197561d88
cairon-henrique-60 <caironhenrique60@gmail.com>
2022-05-05 23:25:47 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Atividade 4 do portifólio do segundo semestre ADS pela Anhanguera.
-// by: Cairon Henrique Gonçalves
 const form = document.getElementById('form')
 const username = document.getElementById('username')
 const email = document.getElementById('email')
